0b705978c51e235373cb7ed32177dcd0a2e54723
Nikolay Jeliazkov <nijel8@gmail.com>
2017-10-29 14:19:22 -0400

    Lower battery IRQ_VOLT_EMPTY and CUTOFF_VOLTAGE
    *back to original kenzo values before Xiaomi changed them for Mi Max kernel source release
    *fixes sudden battery capacity drop to zero thus causing early shutdown when battery still can handle the power demand
    *also make SoC battery data logging more user friendly

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -2066,7 +2066,8 @@ static void soc_work_fn(struct work_struct *work)
 {	struct fg_chip *chip = container_of(work,
 				struct fg_chip,
 				soc_work.work);
-	pr_info("adjust_soc: s %d r %d i %d v %d t %d\n",
+	pr_info("adjust_soc: BATTERY: Capacity %d%%, Charge %dµAh, "
+                        "Current %dµA, Voltage %dµV, Temperature %dc°C\n",
 			get_prop_capacity(chip),
 			get_sram_prop_now(chip, FG_DATA_BATT_SOC),
 			get_sram_prop_now(chip, FG_DATA_CURRENT),
